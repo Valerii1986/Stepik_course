3cdf192ac5ff0ad787ff987e3b0dea1ee49325ed
Valerii Slobodianiuk <valerii.slobodianiuk@gmail.com>
2024-11-28 15:26:39 +0700

    part 9.7.4 resolve comments

--- Part9/Part9.7/Part9.7.4.py
+++ Part9/Part9.7/Part9.7.4.py
@@ -2,29 +2,16 @@
 # Напишите программу, которая принимает 4 слова и находит среди них самое тяжелое слово.
 # Если самых тяжелых слов будет несколько, то программа должна вывести первое из них.
 
-word_1 = input()
-word_2 = input()
-word_3 = input()
-word_4 = input()
-counter_1 = 0
-counter_2 = 0
-counter_3 = 0
-counter_4 = 0
-for char in word_1:
-    counter_1 += ord(char)
-for char in word_2:
-    counter_2 += ord(char)
-for char in word_3:
-    counter_3 += ord(char)
-for char in word_4:
-    counter_4 += ord(char)
-if max(counter_1, counter_2, counter_3, counter_4) == counter_1:
-    print(word_1)
-elif max(counter_1, counter_2, counter_3, counter_4) == counter_2:
-    print(word_2)
-elif max(counter_1, counter_2, counter_3, counter_4) == counter_3:
-    print(word_3)
-elif max(counter_1, counter_2, counter_3, counter_4) == counter_4:
-    print(word_4)
+maxi = 0
+for _ in range(4):
+    summ = 0
+    text = input()
+    for i in text:
+        n = ord(i)
+        summ += n
+    if summ > maxi:
+        maxi = summ
+        name = text
+print(name)
 
 
