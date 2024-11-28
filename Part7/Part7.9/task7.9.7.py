f09df18a6ff0f7d1c5a71a0d2c3ef7caa54a36aa
Valerii Slobodianiuk <valerii.slobodianiuk@gmail.com>
2024-11-28 15:07:13 +0700

    part 7.9 resolve comments

--- Part7/Part7.9/task7.9.7.py
+++ Part7/Part7.9/task7.9.7.py
@@ -1,14 +1,12 @@
 # На вход программе подается два натуральных числа a и b (a< b). Напишите программу, которая находит все
 # простые числа от a до b включительно.
 
-num_a = int(input())
-num_b = int(input())
-counter = 0
+num_a, num_b = int(input()), int(input())
 for i in range(num_a, num_b + 1):
+    if i == 1:
+        continue
     for j in range(2, i):
-        counter = 0
         if i % j == 0:
-            counter = 1
             break
-    if counter == 0 and i != 1:
+    else:
         print(i)
