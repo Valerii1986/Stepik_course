f143c79ba8b8052046646237681d91fe7260281f
Valerii Slobodianiuk <valerii.slobodianiuk@gmail.com>
2025-01-11 16:59:03 +0800

    update Part13.5.1

--- Part13/Part13.5/task13.5.1.py
+++ Part13/Part13.5/task13.5.1.py
@@ -3,10 +3,8 @@
 # или False в противном случае.
 
 def is_valid_triangle(side1, side2, side3):
-    if not (side1 + side2 <= side3 or side1 + side3 <= side2 or side2 + side3 <= side1):
-        return True
-    else:
-        return False
+    return side1 < side2 + side3 and side2 < side3 + side1 and side3 < side1 + side2
 
 
 print(is_valid_triangle(int(input()), int(input()), int(input())))
+
